c76d71c7577557e0b9553ced796b5c398988dcb6
KrishShah0505 <krishshah0505@gmail.com>
2025-02-27 10:30:09 +0530

    Updated search bar to dynamically reset todos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-
 import './App.css';
-import Header from "./My Components/Header"
-import Todos from "./My Components/Todos"
-import Footer from "./My Components/Footer"
+import Header from "./My Components/Header";
+import Todos from "./My Components/Todos";
+import Footer from "./My Components/Footer";
 import AddTodo from './My Components/AddTodo';
 import React, { useState, useEffect } from "react";
 
@@ -10,53 +9,41 @@ function App() {
   let initTodo;
   if (localStorage.getItem("todos") === null) {
     initTodo = [];
-
-  }
-  else {
+  } else {
     initTodo = JSON.parse(localStorage.getItem("todos"));
   }
-  const onDelete = (todo) => {
-    console.log("I am ondelete of todo", todo)
-    // let index=todos.indexOf(todo);
-    //todos.splice(index,1)
-    settodos(todos.filter((e) => {
-      return e !== todo;
-    }))
+
+  const [todos, setTodos] = useState(initTodo);
+  const [filteredTodos, setFilteredTodos] = useState(initTodo);
+
+  useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
-  }
+    setFilteredTodos(todos);
+  }, [todos]);
+
+  const onDelete = (todo) => {
+    console.log("Deleting todo:", todo);
+    setTodos(todos.filter((e) => e !== todo));
+  };
+
   const addTodo = (title, desc) => {
-    console.log("I am adding this todo", title, desc)
-    let sno;
-    if (todos.length === 0) {
-      sno = 0;
-    }
-    else {
-      sno = todos[todos.length - 1].sno + 1
-    }
+    console.log("Adding todo:", title, desc);
+    let sno = todos.length === 0 ? 0 : todos[todos.length - 1].sno + 1;
+
     const myTodo = {
       sno: sno,
       title: title,
       desc: desc
-    }
-    settodos([...todos, myTodo]);
-    console.log(myTodo)
+    };
 
+    setTodos([...todos, myTodo]);
+  };
 
-
-
-  }
-  const [todos, settodos] = useState(initTodo);
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-
-
-  }, [todos]
-  )
   return (
     <>
-      <Header title="My Todos List" searchBar={true} />
+      <Header title="My Todos List" searchBar={true} todos={todos} setFilteredTodos={setFilteredTodos} />
       <AddTodo addTodo={addTodo} />
-      <Todos todos={todos} onDelete={onDelete} />
+      <Todos todos={filteredTodos} onDelete={onDelete} />
       <Footer />
     </>
   );
